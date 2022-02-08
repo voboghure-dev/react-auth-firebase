3cd52dd450ac7d34dd8ad37d66e30096bbf3123e
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-08 17:31:59 +0600

    Auth context setup
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,13 @@
 import { useRef } from 'react';
 import { Card, Form, Button } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+
   return (
     <>
       <Card>
