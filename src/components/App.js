b7a0a3d4891b084d75c69b32e0d1b039a426d518
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-09 01:18:36 +0600

    Setup Route v6
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
 import Signup from './Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className='d-flex align-items-center justify-content-center'
-        style={{ minHeight: '100vh' }}
-      >
-        <div className='w-100' style={{ maxWidth: '400px' }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path='/' element={<Dashboard />} />
+              <Route path='/signup' element={<Signup />} />
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   );
 }
 
