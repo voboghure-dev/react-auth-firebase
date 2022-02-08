a0ebcea835729b82586ef5233e1a413fc3a63538
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-09 00:43:35 +0600

    Auth loading state
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,9 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { auth } from '../firebase';
-import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from 'firebase/auth';
 
 const AuthContext = React.createContext();
 
@@ -10,6 +13,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
 
   function signup(email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -18,6 +22,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setCurrentUser(user);
+      setLoading(false);
     });
 
     return unsubscribe;
@@ -25,5 +30,9 @@ export function AuthProvider({ children }) {
 
   const value = { currentUser, signup };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
