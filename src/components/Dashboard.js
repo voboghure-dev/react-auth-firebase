ddac3ef654d6bdc214f9c15841a7a3591364d94c
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-09 15:24:10 +0600

    Update dashboard component
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,31 @@
+import { useState } from 'react';
+import { Button, Card, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const [error, setError] = useState('');
+  const { currentUser } = useAuth();
+
+  function handleLogout() {}
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Profile</h2>
+          {error && <Alert variant='danger'>{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to='/update-profile' className='btn btn-primary w-100 mt-3'>
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant='link' onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
 }
